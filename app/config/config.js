efd7e0876501b22d570970e19b1715d66bfff8aa
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2024-12-17 00:43:27 +0600

    app.js || api.js || config.js add

---
+++ app/config/config.js
@@ -0,0 +1,16 @@
+export const PORT = 5000;
+
+export const EMAIL_HOST = "sandbox.smtp.mailtrap.io";
+export const EMAIL_PORT = 2525;
+export const EMAIL_SECURITY = false;
+export const EMAIL_USER = "ffd3655ea4cb6f";
+export const EMAIL_PASS = "9eec6de13aba90";
+export const EMAIL_FROM = "<info@teamemil.com>";
+// export const EMAIL_UN_AUTH = false;
+
+export const WEB_CACHE = false;
+export const MAX_JSON_SIZE = "10MB";
+export const URL_ENCODE = "true";
+
+export const REQUEST_TIME = 20*60*1000;
+export const REQUEST_NUMBER = 2000;
